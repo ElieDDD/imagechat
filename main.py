8ea7a4b82c8337fb1fe541a061258bd8ac360993
ElieDDD <30892423+ElieDDD@users.noreply.github.com>
2025-01-06 17:59:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if os.path.exists(IMAGE_FOLDER):
             blurred_image = img.filter(ImageFilter.GaussianBlur(15))  # Apply strong blur
             blurred_image.save("blurred_image.png")  # Save the blurred image for display
             #st.image(blurred_image, caption='Blurred Randomly Selected Image')
-            st.image(blurred_image,"", 500)
+            st.image(blurred_image,"", 200)
         # Encode the blurred image
         base64_image = encode_image("blurred_image.png")
 
