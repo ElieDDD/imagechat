63d2396b6cbceff6968584657dd86c8447895b15
ElieDDD <30892423+ElieDDD@users.noreply.github.com>
2025-01-06 18:01:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if os.path.exists(IMAGE_FOLDER):
             messages=[
                 {"role": "system", "content": "You are a helpful assistant that responds in Markdown."},
                 {"role": "user", "content": [
-                    {"type": "text", "text": "Could you describe the image and create a narrative to highlight important details and provide recommendations. Create a narrative with Observations, important details, and ideas for further analysis with bullet points."},
+                    {"type": "text", "text": "Could you describe the image and create a narrative to highlight important details and provide recommendations. Provide Observations, important details, evidence for resistance, and ideas for further analysis with bullet points."},
                     {"type": "image_url", "image_url": {
                         "url": f"data:image/png;base64,{base64_image}"}
                     }
